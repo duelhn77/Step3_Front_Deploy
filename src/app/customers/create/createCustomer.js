a063aaa1bca5b64348953c95fc708e04a1293705
duelhn77 <duelhn77@gmail.com>
2025-02-09 06:10:42 +0900

    Add files via upload

---
+++ src/app/customers/create/createCustomer.js
@@ -0,0 +1,45 @@
+"use server";
+import { revalidatePath } from "next/cache";
+
+const createCustomer = async (formData) => {
+  const creating_customer_name = formData.get("customer_name");
+  const creating_customer_id = formData.get("customer_id");
+  const creating_age = formData.get("age");
+  const creating_gender = formData.get("gender");
+
+
+  // バリデーションチェック
+  if (!creating_customer_name) {
+    throw new Error("名前が未入力です");
+  }
+  if (!creating_customer_id.trim()) {
+    throw new Error("顧客IDが未入力です");
+  }
+  if (!creating_age.trim()) {
+    throw new Error("年齢が未入力です");
+  }
+  if (!creating_gender.trim()) {
+    throw new Error("性別が未入力です");
+  }
+
+
+  const body_msg = JSON.stringify({
+    customer_name: creating_customer_name,
+    customer_id: creating_customer_id,
+    age: creating_age,
+    gender: creating_gender,
+  });
+
+  const res = await fetch(`http://127.0.0.1:8000/customers`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: body_msg,
+  });
+  if (!res.ok) {
+    throw new Error("Failed to create customer");
+  }
+
+  revalidatePath(`/customers`);
+};
+
+export default createCustomer;
