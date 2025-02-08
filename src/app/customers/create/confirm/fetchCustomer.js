f40522fb42f632606cfcafc1928adc998fdfbee0
duelhn77 <duelhn77@gmail.com>
2025-02-09 07:59:24 +0900

    Add files via upload

--- src/app/customers/create/confirm/fetchCustomer.js
+++ src/app/customers/create/confirm/fetchCustomer.js
@@ -1,5 +1,5 @@
 export default async function fetchCustomer(id) {
-  const res = await fetch(`http://127.0.0.1:8000/customers?customer_id=${id}`, {
+  const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + `/customers?customer_id=${id}`, {
     cache: "no-cache",
   });
   if (!res.ok) {
