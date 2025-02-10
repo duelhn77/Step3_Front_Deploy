370d5f504dc8f34d11bf3a4feb974710f3cdfa39
duelhn77 <duelhn77@gmail.com>
2025-02-10 20:28:31 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 require('dotenv').config()
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'standalone'
+// }
 const nextConfig = {
-    output: 'standalone'
+    output: 'standalone',
+    env: {
+        // Reference a variable that was defined in the .env file and make it available at Build Time
+        API_ENDPOINT: process.env.API_ENDPOINT,
+      },
 }
 
 module.exports = nextConfig
